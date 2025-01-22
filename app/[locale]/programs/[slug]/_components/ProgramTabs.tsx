7254880f71dab6942ac396ae2cc39d1f6b7aa758
mo7amed4a <m7md0a99@gmail.com>
2025-01-22 04:19:11 +0200

    edit overview

--- app/[locale]/programs/[slug]/_components/ProgramTabs.tsx
+++ app/[locale]/programs/[slug]/_components/ProgramTabs.tsx
@@ -16,36 +16,39 @@ export default async function ProgramTabs({ children , locale, overview, housing
   const {program} = await getTranslations(lng);
   const {home:dataLangHome} = await getTranslations(lng);
   return (
-    <Tabs
-      defaultValue="programs"
-      className="w-full"
-      dir={lng === "ar" || lng === "fa" ? "rtl" : "ltr"}
-    >
-      <TabsList className="mb-4 gap-4 flex justify-start w-full bg-transparent">
-        <TabsTrigger
-          value="programs"
-          className="bg-transparent border rounded-full px-3 py-1.5"
-        >
-          {housings || program.title}
-        </TabsTrigger>
-        {overview && <TabsTrigger
-          value="overview"
-          className="bg-transparent border rounded-full px-3 py-1.5"
-        >
-          {program.overview}
-        </TabsTrigger>}
-      </TabsList>
-      <TabsContent value="programs">
-        <div className="flex flex-col lg:flex-row gap-4">
-          {!housings && <div className="lg:w-96">
-            <FilterSelectTwo dataLang={dataLangHome} lng={lng} />
-          </div>}
-          <div className="w-full">{children}</div>
-        </div>
-      </TabsContent>
-      <TabsContent value="overview">
-        {overview && <OverviewContent overview={overview} lng={lng} />}
-      </TabsContent>
-    </Tabs>
+    <>
+      <Tabs
+        defaultValue="programs"
+        className="w-full"
+        dir={lng === "ar" || lng === "fa" ? "rtl" : "ltr"}
+      >
+        <TabsList className="mb-4 gap-4 flex justify-start w-full bg-transparent">
+          <TabsTrigger
+            value="programs"
+            className="bg-transparent border rounded-full px-3 py-1.5"
+          >
+            {housings || program.title}
+          </TabsTrigger>
+          {overview && <TabsTrigger
+            value="overview"
+            className="bg-transparent border rounded-full px-3 py-1.5"
+          >
+            {program.overview}
+          </TabsTrigger>}
+        </TabsList>
+        <TabsContent value="programs">
+          <div className="flex flex-col lg:flex-row gap-4">
+            {!housings && <div className="lg:w-96">
+              <FilterSelectTwo dataLang={dataLangHome} lng={lng} />
+            </div>}
+            <div className="w-full">{children}</div>
+          </div>
+        </TabsContent>
+        <TabsContent value="overview">
+          {overview && <OverviewContent overview={overview} lng={lng} />}
+        </TabsContent>
+      </Tabs>
+      {overview && <OverviewContent overview={overview} lng={lng} />}
+    </>
   );
 }
